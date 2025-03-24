43cbb5d63cdcb428aaa383924bc524e66619da84
Omar-Webdeveloper <omarabojolban9@gmail.com>
2025-03-24 08:53:34 +0300

    First is Done (Show + Add )

--- angular_task_today_23_3_2025.client/src/app/app-routing.module.ts
+++ angular_task_today_23_3_2025.client/src/app/app-routing.module.ts
@@ -3,13 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './Voucher/dashboard/dashboard.component';
 import { ShowAllComponent } from './Voucher/show-all/show-all.component';
 import { AddVoucherComponent } from './Voucher/add-voucher/add-voucher.component';
+import { EditVoucherComponent } from './Voucher/edit-voucher/edit-voucher.component';
 
 const routes: Routes = [
   {
     path: 'Admin', component: DashboardComponent, children:
       [
         { path: 'All', component: ShowAllComponent },
-        { path: 'Add', component: AddVoucherComponent }
+        { path: 'Add', component: AddVoucherComponent },
+        { path: 'Edit/:id', component:EditVoucherComponent }
       ]
   }
 ];
