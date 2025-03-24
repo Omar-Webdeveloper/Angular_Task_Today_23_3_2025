fd1c8cfa1c4d750cc27704490a2d957ce36fbd55
Omar-Webdeveloper <omarabojolban9@gmail.com>
2025-03-24 09:58:15 +0300

    Edit is Completed

--- angular_task_today_23_3_2025.client/src/app/Services/service.service.ts
+++ angular_task_today_23_3_2025.client/src/app/Services/service.service.ts
@@ -17,8 +17,8 @@ export class ServiceService {
   Post_Voucher(data: any): Observable<any>  {
     return this.http.post<any>('https://67d9ae8e35c87309f529c67f.mockapi.io/sign/voucher',data);
   }
-  //Get_Voucher() {
-  //  return this.http.get<any>('https://67d9ae8e35c87309f529c67f.mockapi.io/sign/voucher');
-  //}
+  Get_Voucher_based_ID(id:any) {
+    return this.http.get<any>(`https://67d9ae8e35c87309f529c67f.mockapi.io/sign/voucher/${id}`);
+  }
 
 }
--- angular_task_today_23_3_2025.client/src/app/Voucher/edit-voucher/edit-voucher.component.ts
+++ angular_task_today_23_3_2025.client/src/app/Voucher/edit-voucher/edit-voucher.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ServiceService } from '../../Services/service.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-edit-voucher',
@@ -7,5 +9,23 @@ import { Component } from '@angular/core';
   styleUrl: './edit-voucher.component.css'
 })
 export class EditVoucherComponent {
+  Voucher_Containter: any;
+  Vocher_id: any;
+  constructor(private micro_service: ServiceService, private _active: ActivatedRoute) { }
+  ngOnInit() {
+    
+    this.Vocher_id = this._active.snapshot.paramMap.get('id');
+    this.micro_service.Get_Voucher_based_ID(this.Vocher_id).subscribe((data) => { console.log(data); this.Voucher_Containter = data; console.log(this.Voucher_Containter); })
+  }
+  
+ 
+   
+    
+  
+  
+  Edit_Voucher(data: any) {
+   
+    this.micro_service.Put_Voucher(this.Vocher_id, data).subscribe(() => {alert('Edited') })
+  }
 
 }
